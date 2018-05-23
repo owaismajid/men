ad7aab17d2c98bf9719cf3d6051a9861d68463de
OwaisMajid <owais.majid@yahoo.com>
2018-05-24 02:24:39 +0300

    moving controller

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,19 +1,13 @@
 //** API Routes */
 import { Router } from 'express'
 import TodoService from '../services/todo.service';
+import TodoController from '../controllers/todo.controller';
 const r = Router();
 r.get('/', (req, res)=>{
     res.send("get api/")
 })
 
-r.get('/test', (req, res)=>{
-    new TodoService().create({
-        todo: 'test to do',
-        priority: 2
-    })
-    .then(result=> res.send("OK"))
-    .catch(error=> res.send(error))
-})
+r.get('/test', (req, res)=>new TodoController().getAll(req, res))
 
 r.put('/', (req, res)=>{
     res.send("put api/")
@@ -22,3 +16,10 @@ r.delete('/', (req, res)=>{
     res.send("delete api/")
 })
 export { r as ApiRouter }
+
+// new TodoService().create({
+//     todo: 'test to do',
+//     priority: 2
+// })
+// .then(result=> res.send("OK"))
+// .catch(error=> res.send(error))
