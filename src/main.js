808a01dc850e463be2db5c392c049b7895dc954b
OwaisMajid <owais.majid@yahoo.com>
2018-05-24 02:25:00 +0300

    support pug and req body

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import { AppRouter } from './routes/app.routes';
-import { ApiRouter } from './routes/api.routes'
-const app = express();
+import { ApiRouter } from './routes/api.routes';
+import * as bodyParser from 'body-parser';
+import * as path from 'path';
+
+
+const app = express()
+/**
+ * App middelwares
+ */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 /**
  * App routes
  */
@@ -14,3 +27,4 @@ app.use('/api', ApiRouter)
 app.listen(3000, ()=>console.info("Server is Working"))
 
 
+export { app as App}
