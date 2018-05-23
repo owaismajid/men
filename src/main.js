e747fce682fde3c38f8e39d882e8d99694cf505f
OwaisMajid <owais.majid@yahoo.com>
2018-05-23 23:39:45 +0300

    add commentes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import express from 'express';
 import { AppRouter } from './routes/app.routes';
 import { ApiRouter } from './routes/api.routes'
 const app = express();
+/**
+ * App routes
+ */
 app.use("/", AppRouter)
 app.use('/api', ApiRouter)
+
+/**
+ * App server listener
+ */
 app.listen(3000, ()=>console.info("Server is Working"))
 
 
