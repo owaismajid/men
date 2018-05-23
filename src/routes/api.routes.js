ef1a730e27f305b4bd4b93b5eff050171e0057c1
OwaisMajid <owais.majid@yahoo.com>
2018-05-24 00:56:55 +0300

    test TodoService

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,12 +1,20 @@
 //** API Routes */
 import { Router } from 'express'
+import TodoService from '../services/todo.service';
 const r = Router();
 r.get('/', (req, res)=>{
     res.send("get api/")
 })
-r.post('/', (req, res)=>{
-    res.send("post api/")
+
+r.get('/test', (req, res)=>{
+    new TodoService().create({
+        todo: 'test to do',
+        priority: 2
+    })
+    .then(result=> res.send("OK"))
+    .catch(error=> res.send(error))
 })
+
 r.put('/', (req, res)=>{
     res.send("put api/")
 })
