53b70d74b8fe0bd6ef1487800ff833ebbf69b4c2
OwaisMajid <owais.majid@yahoo.com>
2018-05-24 03:47:29 +0300

    not

--- enviroment.js
+++ enviroment.js
@@ -1,5 +1,13 @@
 const env = {
+    app:{
+        name: "",
+        description: "",
+        auther: "",
+        relatedTo: ""
+    },
     db: {
+        domain: "ds229380.mlab.com",
+        port: "29380",
         user: 'owais',
         password: 'owaistest',
         dbname: 'tododatabase'
