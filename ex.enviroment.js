d25032b85cbe50807087c2ebec038e092d5c145a
OwaisMajid <owais.majid@yahoo.com>
2018-05-24 03:41:54 +0300

    init

---
+++ ex.enviroment.js
@@ -0,0 +1,17 @@
+const env = {
+    app:{
+        name: "",
+        description: "",
+        auther: "",
+        relatedTo: ""
+    },
+    db: {
+        domain: "",
+        port: "",
+        user: '',
+        password: '',
+        dbname: ''
+    }
+}
+
+export { env as ENV }
